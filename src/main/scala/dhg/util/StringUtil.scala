596803953dd10c795f23ec5a4d773ded4b7f5cdc
Dan Garrette <dhgarrette@gmail.com>
2013-01-07 00:29:40 -0600

    fixed bug on rtrim for all-whitespace string

--- src/main/scala/dhg/util/StringUtil.scala
+++ src/main/scala/dhg/util/StringUtil.scala
@@ -2,10 +2,11 @@ package dhg.util
 
 object StringUtil {
 
+  val WhitespaceRe = """\s*""".r
   val RTrimRe = """(.*\S)\s*""".r
   implicit class EnrichedString(self: String) {
     def rtrim = self match {
-      case "" => ""
+      case WhitespaceRe() => ""
       case RTrimRe(trimmed) => trimmed
     }
   }
