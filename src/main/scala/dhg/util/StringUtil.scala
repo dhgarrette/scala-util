a4431127f794b6171598e5dbb10078a17db31fe2
Dan Garrette <dhgarrette@gmail.com>
2013-06-07 09:07:32 -0700

    added String.indent methods

--- src/main/scala/dhg/util/StringUtil.scala
+++ src/main/scala/dhg/util/StringUtil.scala
@@ -4,16 +4,17 @@ object StringUtil {
 
   val WhitespaceRe = """\s*""".r
   val RTrimRe = """(.*\S)\s*""".r
+
   implicit class EnrichedString(val self: String) extends AnyVal {
     def rtrim = self match {
       case WhitespaceRe() => ""
       case RTrimRe(trimmed) => trimmed
     }
 
-    def splitlines = self.split("\n")
-    def splitWhitespace = self.split("\\s+")
+    def splitlines: Array[String] = self.split("\n")
+    def splitWhitespace: Array[String] = self.split("\\s+")
 
-    def rsplit(str: String, n: Int) = {
+    def rsplit(str: String, n: Int): Array[String] = {
       val parts = self.split(str)
       val (front, back) = parts.splitAt(parts.size - n + 1)
       if (front.nonEmpty)
@@ -22,7 +23,7 @@ object StringUtil {
         back
     }
 
-    def wrap(width: Int = 80) = {
+    def wrap(width: Int = 80): String = {
       val (completeLines, lastLine) =
         self.split("\\s+").foldLeft((Vector[String](), "")) {
           case ((lines, currLine), tok) =>
@@ -36,6 +37,14 @@ object StringUtil {
       val lines = completeLines :+ lastLine
       //lines.map(s => f"$s%-80s|").mkString("\n")
       lines.mkString("\n")
+    }
+
+    def indent(spaces: Int): String = {
+      indent(" " * spaces)
+    }
+
+    def indent(leftColumn: String): String = {
+      self.split("\n").map(leftColumn + _).mkString("\n")
     }
 
   }
