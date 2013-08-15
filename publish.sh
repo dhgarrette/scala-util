17f9e0dea536e662c11eeb574294a6a723b0b0dc
Dan Garrette <dhgarrette@gmail.com>
2013-08-15 03:00:27 -0700

    publish API to web

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
 sbt publish
 scp target/scala-2.10/scala-util_2.10-1.0.0-SNAPSHOT* k:public_html/maven-repository/snapshots/dhg/scala-util_2.10/1.0.0-SNAPSHOT
 
+sbt doc
+scp -r target/scala-2.10/api k:public_html/maven-repository/snapshots/dhg/scala-util_2.10/1.0.0-SNAPSHOT
